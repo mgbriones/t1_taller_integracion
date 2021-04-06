5fd57fda30594c72731128e68990aa09ae7cdae2
mgbriones <mgbriones@uc.cl>
2021-04-06 13:40:05 -0400

    logre consultar a la api

--- Breakingbad/aplicaciones/urls.py
+++ Breakingbad/aplicaciones/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import home, contacto
+from .views import home, contacto, prueba
 
 urlpatterns = [
     path('', home, name="home"),
-    path('contacto/', contacto, name="contacto")
+    path('contacto/', contacto, name="contacto"),
 ]
--- Breakingbad/aplicaciones/views.py
+++ Breakingbad/aplicaciones/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+import json
 
 # Create your views here.
 def home(request):
     return render(request,'aplicaciones/home.html')
 
+
 def contacto(request):
     return render(request,'aplicaciones/contacto.html')
+
+
+#puede que tenga que borrarla
+
+def api():
+    url = 'https://tarea-1-breaking-bad.herokuapp.com/api/characters/1'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        content = response.content
+        #print("\n\n@@&&========================\nFUNCIONO\n\n")
+        print('\n\n@@@@@@@@@@@@@@@@@@@@@@@')
+        print(content)
+        return content
+
+
+def prueba(request):
+    
+    return HttpResponse(api())
+
+
+
