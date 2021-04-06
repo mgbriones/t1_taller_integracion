50a02049dae243d92206154c10c614034d398af4
mgbriones <mgbriones@uc.cl>
2021-04-06 19:51:41 -0400

    logre pasar info al templates

--- Breakingbad/Breakingbad/urls.py
+++ Breakingbad/Breakingbad/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from aplicaciones.views import prueba
+from aplicaciones.views import prueba, temporadas
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('aplicaciones.urls')),
-    path('prueba/', prueba)
+    path('prueba/', prueba),
+    path('temporadas/', temporadas),
+    
 ]
--- Breakingbad/aplicaciones/views.py
+++ Breakingbad/aplicaciones/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 import json
+from django.template import Context, Template
 
 # Create your views here.
 def home(request):
+    
     return render(request,'aplicaciones/home.html')
 
 
 def contacto(request):
+    print("##########################")
+    print(request)
     return render(request,'aplicaciones/contacto.html')
 
 
@@ -19,10 +23,11 @@ def api():
     response = requests.get(url)
 
     if response.status_code == 200:
-        content = response.content
+        content = response.json()
         #print("\n\n@@&&========================\nFUNCIONO\n\n")
         print('\n\n@@@@@@@@@@@@@@@@@@@@@@@')
         print(content)
+        print(content[0])
         return content
 
 
@@ -30,5 +35,25 @@ def prueba(request):
     
     return HttpResponse(api())
 
+def temporadas(request):
+
+    #se carga el template
+    #doc_externo = open("C:/Users/Matias Briones/Desktop/t1_taller_integracion/Breakingbad/aplicaciones/templates/aplicaciones/temporadas.html")
+    doc_externo = open("../Breakingbad/aplicaciones/templates/aplicaciones/temporadas.html")
+    plt = Template(doc_externo.read()) #se lee el template
+    doc_externo.close()
+
+    #ctx = Context()
+    algo = api()
+    print("PASO X AQUI")
+    print(algo)
+    print('============')
+    ctx = Context({"consulta": algo, "papa":"padre"})
+
+    documento = plt.render(ctx)
+
+
+
+    return HttpResponse(documento)
 
 
